e4267d34a63b3e0ea2d662d0b50bb45b53a924c0
kevin xie <xie.kevin.j@gmail.com>
2021-01-16 02:04:58 -0500

    binary search fun

--- linearSearch.js
+++ linearSearch.js
@@ -15,3 +15,21 @@
 // Binary search
 // You can eliminate half of the remaining elements at a time
 // only works on sorted arrays
+// create a left pointer at the start of the array, right pointer at the end
+// and a middle pointer..
+//  create a pointer in the middle, value too small? move left pointer up
+// value too large, move the right pointer down
+// never find value return -1
+
+function binarySearch(arr, val) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== val && start <= end) {
+    if (val < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[middle] === val ? middle : -1;
+}
