fab42d1ae3086b4780f4393cf7a8a57dda438c67
kevin xie <xie.kevin.j@gmail.com>
2021-01-25 18:51:23 -0500

    citi challenge

--- citiChallenge.js
+++ citiChallenge.js
@@ -1,5 +1,11 @@
-function solution(a) {
-  // find the longest chain of consecutive nums
-  for (let i = 0; i < a.length; i++) {}
-  // flip the ones before/after the chain + count it.
+// function solution(a) {
+//   // find the longest chain of consecutive nums
+//   for (let i = 0; i < a.length; i++) {}
+//   // flip the ones before/after the chain + count it.
+// }
+
+function coins(a) {
+  let newArr = a.slice(1);
+  if (a[0] === 1) {
+  }
 }
